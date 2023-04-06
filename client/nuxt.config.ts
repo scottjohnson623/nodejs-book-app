9662535501e6341596e43de2b59c4ef7edfdb05e
Scott Johnson <scott.johnson623@gmail.com>
2023-04-06 12:09:43 +0900

    Install vuetify, change base page to vuetify wireframe

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,5 +1,8 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
-
+    css: ['vuetify/lib/styles/main.sass'],
+    build: {
+        transpile: ['vuetify'],
+      },    
 })
