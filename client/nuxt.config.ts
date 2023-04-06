bb6ac67fb904384a8acd092dd3554ac7171adc25
Scott Johnson <scott.johnson623@gmail.com>
2023-04-06 12:18:01 +0900

    Reinitialize repository with just nuxt client

--- client/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineNuxtConfig } from 'nuxt/config'
-
-export default defineNuxtConfig({
-    css: ['vuetify/lib/styles/main.sass'],
-    build: {
-        transpile: ['vuetify'],
-      },    
-})
