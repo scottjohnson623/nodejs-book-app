5d6ebc61331ba68971106301a2ba29619a2438c6
Scott Johnson <scott.johnson623@gmail.com>
2023-04-07 10:47:07 +0900

    Install vuetify, add image to test Cloudfront cache, redo home page

--- app.vue
+++ app.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      model-value
+      class="pt-4"
+      color="grey-lighten-3"
+      rail
+    >
+      <v-avatar
+        v-for="n in 6"
+        :key="n"
+        :color="`grey-${n === 1 ? 'darken' : 'lighten'}-1`"
+        :size="n === 1 ? 36 : 20"
+        class="d-block text-center mx-auto mb-9"
+      ></v-avatar>
+    </v-navigation-drawer>
+
+    <v-main>
+      <img src="~/public/IMG_6530.jpg" width="400" height="400" class="text-center"/>
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+  export default {
+    //
+  }
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,8 @@ export default defineNuxtConfig({
     nitro: {
         preset: 'aws-lambda',
       },
+    css: ['vuetify/lib/styles/main.sass'],
+    build: {
+    transpile: ['vuetify'],
+    },
 })
